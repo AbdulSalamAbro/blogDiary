dcb7d621df9b22c642b94c9a2810d41074f0f973
Abdul Salam <64842392+AbdulSalamAbro@users.noreply.github.com>
2022-08-13 22:57:12 +0900

    Update App.js

--- Frontend/App.js
+++ Frontend/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Auth from './components/Auth/Auth';
 const App = () => (
+    //so here we define the routes for our pages like home page and login auth
     <BrowserRouter>
       <Container maxWidth="lg">
         <Navbar />
