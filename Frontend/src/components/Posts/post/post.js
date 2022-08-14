29871fcff36e55e751f981fbe46a6403889ee455
Rizwaniba <79909102+Rizwaniba@users.noreply.github.com>
2022-08-14 08:49:29 +0500

    post file code

--- Frontend/src/components/Posts/post/post.js
+++ Frontend/src/components/Posts/post/post.js
@@ -9,3 +9,56 @@ import moment from 'moment';
 
 import { likePost, deletePost } from '../../../actions/posts';
 import useStyles from './styles';
+
+const Post = ({ post, setCurrentId }) => {
+    const dispatch = useDispatch();
+    const classes = useStyles();
+    const user = JSON.parse(localStorage.getItem('profile'));
+  
+    const Likes = () => {
+      if (post.likes.length > 0) {
+        return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id))
+          ? (
+            <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}` }</>
+          ) : (
+            <><ThumbUpAltOutlined fontSize="small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
+          );
+      }
+  
+      return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>;
+    };
+  
+    return (
+      
+      <div className="container bg-light m-5">
+      <div className="row ">
+          <div className="col-md-10">
+            
+          <div classname="card">
+    <div classname="card-body">
+      <h3 classname="card-title">{post.name}</h3>
+      <p classname="card-text">{post.title}</p>
+      <p classname="card-text">{post.message}</p>
+      <p classname="card-text"><small classname="text-muted">{moment(post.createdAt).fromNow()}</small></p>
+    </div>
+    <img src={post.selectedFile} alt="BigCo Inc. logo"/>
+    <CardActions className={classes.cardActions}>
+          <Button size="small" color="primary" disabled={!user?.result} onClick={() => dispatch(likePost(post._id))}>
+            <Likes />
+        </Button>
+          {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+          <Button size="small" color="secondary" onClick={() => dispatch(deletePost(post._id))}>
+             <DeleteIcon fontSize="small" /> Delete
+           </Button>
+           )}
+         </CardActions>
+  
+    
+  </div>
+          </div>
+      </div>
+  </div>
+      );
+  };
+  
+  export default Post;
