5cdd04898ccdd22c393ead36f241265c8974ac51
Rizwaniba <79909102+Rizwaniba@users.noreply.github.com>
2022-08-14 08:32:58 +0500

    home code

--- Frontend/src/components/Home/Home.js
+++ Frontend/src/components/Home/Home.js
@@ -5,3 +5,27 @@ import { useDispatch } from 'react-redux';
 import { getPosts } from '../../actions/posts';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
+
+const Home = () => {
+    const [currentId, setCurrentId] = useState(0);
+    const dispatch = useDispatch();
+  
+    useEffect(() => {
+      dispatch(getPosts());
+    }, [currentId, dispatch]);
+  
+    return (
+      <>
+         <Form currentId={currentId} setCurrentId={setCurrentId} />
+         
+              <Posts setCurrentId={setCurrentId} />
+          
+            
+            
+        
+              </> 
+    );
+  };
+  
+  export default Home;
+  
