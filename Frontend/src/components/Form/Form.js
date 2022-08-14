04c44fe46f01959b64c1e3fbb7c4ab22c201a3f4
Rizwaniba <79909102+Rizwaniba@users.noreply.github.com>
2022-08-14 08:29:30 +0500

    Form commit

---
+++ Frontend/src/components/Form/Form.js
@@ -0,0 +1,4 @@
+import React, { useState, useEffect } from 'react';
+import { TextField, Button, Typography, Paper } from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
+import FileBase from 'react-file-base64';
